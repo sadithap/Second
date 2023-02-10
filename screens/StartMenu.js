3e092e114c65f928f137a3df6067ca250973058b
I Gede Ketut Saditha Putra <sadithaputra5@gmail.com>
2023-02-10 20:57:59 +0800

    init

---
+++ screens/StartMenu.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const StartMenu = () => {
+  return (
+    <View>
+      <Text>StartMenu</Text>
+    </View>
+  )
+}
+
+export default StartMenu
+
+const styles = StyleSheet.create({})
